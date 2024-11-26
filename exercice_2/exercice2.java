1e0c78b9985010150859c2ec654925c72b765b27
wafec@3il.fr <christwafe@gmail.com>
2024-11-26 17:59:07 +0100

    exo2 modifié

--- exercice_2/exercice2.java
+++ exercice_2/exercice2.java
@@ -1,21 +1,53 @@
-public class GestionnaireNotes {
 
-    // Affiche note et moyennes
+public class GestionnaireNotes {
+     
+    private String nomEtudiant;
+    private int [] notes;
+
+    /**
+     * Affiche les notes d'un étudiant ainsi que sa moyenne.
+     *
+     * @param nomEtudiant Le nom de l'étudiant.
+     * @param notes       Un tableau contenant les notes de l'étudiant.
+     */
     public void afficherNotesEtMoyenne(String nomEtudiant, int[] notes) {
+       
         // Affichage des notes
+        afficherNotes(nomEtudiant, notes);
+
+        // Calcul et affichage de la moyenne
+        double moyenne = calculerMoyenne(notes);
+        
+        System.out.println("Moyenne : " + moyenne);
+    }
+
+    /**
+     * Affiche les notes d'un étudiant.
+     *
+     * @param nomEtudiant Le nom de l'étudiant.
+     * @param notes  Un tableau contenant les notes.
+     */
+    private void afficherNotes(String nomEtudiant, int[] notes) {
+
         System.out.println("Notes de " + nomEtudiant + ":");
         for (int note : notes) {
             System.out.println("- " + note);
         }
+    }
 
-        // Calcul de la somme des notes
+    /**
+     * Calcule la moyenne des notes.
+     *
+     * @param notes Un tableau contenant les notes.
+     * @return La moyenne des notes.
+     */
+    private double calculerMoyenne(int[] notes) {
         int somme = 0;
         for (int note : notes) {
             somme += note;
         }
-
-        // Affichage de la moyenne
-        double moyenne = (double) somme / notes.length;
-        System.out.println("Moyenne : " + moyenne);
+        return (double) somme / notes.length;
     }
+
+   
 }
